83172bb9615dae82d0e1f36d33953b6dc4284653
thiagoferrari <thiagofcjr@gmail.com>
2020-12-28 10:50:09 -0300

    Estado e Imutabilidade

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,19 +2,46 @@ import React, { Component } from 'react'
 
 class TechList extends Component {
     // aqui posso usar constructor, mas usamos plugin loader no babel [+clean]
-    state = {
+    state = { /* state É IMUTÁVEL (até nas propriedades) */
+        newTech: '',
         techs: ['Node.js', 'ReactJS', 'React Native']
     }
 
-    render() {
-        console.log('this.state :>> ', this.state);
 
-        return (
-            <ul>
-                <li>Node.js</li>
-                <li>ReactJS</li>
-                <li>React Native</li>
-            </ul>
+    /**
+     * abaixo PRECISAMOS de arrow function para ter acesso ao this (var. internas)
+     */
+    handleInputChange = e => {
+        //setState vai setar novos valores no state
+        this.setState({ newTech: e.target.value }) // target.value aqui é o valor dentro do input
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+
+        /**
+         * NÃO PODEMOS USAR PUSH PARA ACRENCENTAR NO STATE !
+         * VAMOS CLONAR O STATE: SPREAD OPERATOR + NOVO VALOR
+         */
+        this.setState({
+            techs: [...this.state.techs, this.state.newTech],
+            newTech: '' // vai voltar no html um input limpo
+        })
+    }
+
+    render() {
+        console.log('techs :>> ', this.state.techs);
+        return ( // aqui precisamos de um container e <> é um, técnica chamada fragment
+            <form onSubmit={this.handleSubmit}>
+                <ul>
+                    {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                </ul>
+                <input type='text'
+                    onChange={this.handleInputChange}
+                    value={this.state.newTech}
+                />
+                <button type='submit'>Enviar</button>
+            </form>
         )
     }
 }

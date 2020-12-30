dff7096e801f7bb2e4630d4fde915e223016794e
thiagoferrari <thiagofcjr@gmail.com>
2020-12-30 09:03:14 -0300

    Ciclo de vida do componente [lifecycle]

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -6,9 +6,34 @@ class TechList extends Component {
     // aqui posso usar constructor, mas usamos plugin loader no babel [+clean]
     state = { /* state É IMUTÁVEL (até nas propriedades) */
         newTech: '',
-        techs: ['Node.js', 'ReactJS', 'React Native']
+        techs: []
     }
 
+    // exec quando componente aparece em tela
+    componentDidMount() {
+        //carregando array prontas em localStorage:
+        const techs = localStorage.getItem('techs')
+
+        if (techs) {
+            this.setState({ techs: JSON.parse(techs) })
+        }
+    }
+
+    // exec sempre que houver alterações nas props ou estado
+    componentDidUpdate(_, prevState) {
+        // verif. se prevState (previous State) é diferente state.techs atual (se teve update)
+        if (prevState.techs !== this.state.techs) {
+
+            // salvando array no localStorage:
+            localStorage.setItem('techs', JSON.stringify(this.state.techs))
+        }
+
+    }
+
+    // exec quando o componente deixa de existir
+    componentWillMount() {
+
+    }
 
     /**
      * abaixo PRECISAMOS de arrow function para ter acesso ao this (var. internas)
@@ -18,13 +43,12 @@ class TechList extends Component {
         this.setState({ newTech: e.target.value }) // target.value aqui é o valor dentro do input
     }
 
+    /**
+    * NÃO PODEMOS USAR PUSH PARA ACRENCENTAR NO STATE ! VAMOS CLONAR O STATE: SPREAD OPERATOR + NOVO VALOR
+    */
     handleSubmit = e => {
         e.preventDefault()
 
-        /**
-         * NÃO PODEMOS USAR PUSH PARA ACRENCENTAR NO STATE !
-         * VAMOS CLONAR O STATE: SPREAD OPERATOR + NOVO VALOR
-         */
         this.setState({
             techs: [...this.state.techs, this.state.newTech],
             newTech: '' // vai voltar no html um input limpo
@@ -41,6 +65,10 @@ class TechList extends Component {
         this.setState({ techs: this.state.techs.filter(t => t !== tech) })
     }
 
+    /**
+     * APRENDIZADO: AS FUNÇÕES QUE VÃO MANIPULAR O STATE PRECISAM ESTAR NO MESMO ARQUIVO QUE O STATE ESTÁ,
+     *  E SE PRECISAR USAR ESSA FUNÇÃO EM OUTRO COMPONENTE, PASSAR COMO PROPS. (PARÂMETROS)
+     */
     render() {
         console.log('techs :>> ', this.state.techs);
         return (
@@ -55,8 +83,6 @@ class TechList extends Component {
                             onDelete={() => this.handleDelete(tech)}
                         />
                     ))}
-                    <TechItem />
-
                 </ul>
 
                 <input type='text'
@@ -70,9 +96,6 @@ class TechList extends Component {
     }
 }
 
-/**
- * APRENDIZADO: AS FUNÇÕES QUE VÃO MANIPULAR O STATE PRECISAM ESTAR NO MESMO ARQUIVO QUE O STATE ESTÁ,
- *  E SE PRECISAR USAR ESSA FUNÇÃO EM OUTRO COMPONENTE, PASSAR COMO PROPS. (PARÂMETROS)
- */
+
 
 export default TechList
